0d72141645456db2e91bf0034023d6b66db27065
Prasad790-sys <psa19974@gmail.com>
2024-09-15 21:25:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function getFormvalue() {
     //Write your code here
-
+     const fn=document.getElementById("fname")
+	 const sn=document.getElementById("sname")
+	 alert(fn.value+" "+sn.value);
 }
